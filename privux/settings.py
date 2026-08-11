69c0d553535e0343d570c8685d7289c98e829a20
Miao Hu <miao.hu@soton.ac.uk>
2026-08-11 13:03:46 +0100

    update privux/settings.py

--- privux/settings.py
+++ privux/settings.py
@@ -88,6 +88,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-e+dfmqm))kmz9k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "True").lower() in {"1", "true", "yes", "on"}
 
+# adding allow_host_ip
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "10.22.12.250", "UOS-24012.ecs.soton.ac.uk", "dips.soton.ac.uk"]
 
 JAZZMIN_SETTINGS = {
